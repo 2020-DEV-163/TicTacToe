56c7dd0423d945db5488ba2762551a83c6f368dd
K D <cod4ejava.kata@gmail.com>
2020-10-13 00:10:26 +0200

    add Play class

---
+++ src/main/java/com/kata/game/tictactoe/PlayTicTacToe.java
@@ -0,0 +1,13 @@
+package com.kata.game.tictactoe;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+import com.kata.game.tictactoe.CellOccupiedException;
+import com.kata.game.tictactoe.InvalidCellRangeException;
+
+public class PlayTicTacToe {
+
+	private final static Logger LOGGER = Logger.getLogger(PlayTicTacToe.class.getName());
+
+}
