94466573a5cad184da1bac6f581d2009973f908a
K D <cod4ejava.kata@gmail.com>
2020-10-13 03:28:34 +0200

    adding Game class and its tests

---
+++ src/main/java/com/kata/game/tictactoe/Game.java
@@ -0,0 +1,53 @@
+package com.kata.game.tictactoe;
+
+import com.kata.game.tictactoe.GameBoard;
+import com.kata.game.tictactoe.CellOccupiedException;
+import com.kata.game.tictactoe.GameResultEnum;
+import com.kata.game.tictactoe.InvalidCellRangeException;
+
+public class Game {
+	GameBoard gameBoard;
+
+	public Game() {
+		gameBoard = new GameBoard();
+	}
+
+	public char getPlayerAt(final int row, final int column) {
+		return gameBoard.getSymbolAt(row, column);
+	}
+
+	public String play(final int row, final int column) throws CellOccupiedException, InvalidCellRangeException {
+
+		validateInputsToPlayFurther(row, column);
+
+		gameBoard.drawSymbolAt(row, column);
+
+		if (isWinner()) {
+			return getCurrentPlayer() + GameResultEnum.WINS.value;
+		} else if (gameBoard.isCellsFullyOccupiedBySymbols()) {
+			return GameResultEnum.DRAW.value;
+		}
+		return GameResultEnum.CONTINUES.value;
+	}
+
+	private void validateInputsToPlayFurther(final int row, final int column)
+			throws InvalidCellRangeException, CellOccupiedException {
+		if (!gameBoard.isValidCellRange(row, column)) {
+			throw new InvalidCellRangeException(
+					"Given cell is out of range, Please select any valid cell in the range of 0 to 2");
+		} else if (!gameBoard.isCellEmpty(row, column)) {
+			throw new CellOccupiedException(
+					"Given cell is not empty as it is already occupied by another symbol, Please select any other valid cell");
+		}
+	}
+
+	boolean isWinner() {
+		return gameBoard.isAnyHorizontalCellsMarkedBySameSymbol() || gameBoard.isAnyVerticalCellsMarkedBySameSymbol()
+				|| gameBoard.isLeftTopToRightBottomDiagonalCellsAreMarkedBySameSymbol()
+				|| gameBoard.isRightTopToLeftBottomDiagonalCellsAreMarkedBySameSymbol();
+	}
+
+	private char getCurrentPlayer() {
+		return gameBoard.getCurrentSymbol();
+	}
+}
